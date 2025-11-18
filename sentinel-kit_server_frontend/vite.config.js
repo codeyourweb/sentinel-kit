0ace3a75101292888c87e84bc1273eebf19e3c9a
codeyourweb <dev.jp.garnier@gmail.com>
2025-11-18 22:48:26 +0100

    node dependencies refresh - monaco editor

--- sentinel-kit_server_frontend/vite.config.js
+++ sentinel-kit_server_frontend/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import monacoEditorPlugin from 'vite-plugin-monaco-editor';
-import { languages } from 'monaco-editor';
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), monacoEditorPlugin({languages: ['yaml']} )],
+  plugins: [vue(), tailwindcss()],
   server: {
     host: '0.0.0.0',
     allowedHosts: process.env.VITE_ALLOWED_HOSTS ? process.env.VITE_ALLOWED_HOSTS.split(',') : ['localhost', '127.0.0.1'],
